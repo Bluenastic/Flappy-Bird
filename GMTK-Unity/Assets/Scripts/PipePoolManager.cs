04722a952b1b37e7675a5bbb1e9b3bca82fd9863
Xanh <xuananh7029@gmail.com>
2024-07-15 23:02:00 +0700

    Flappy_Bird

---
+++ GMTK-Unity/Assets/Scripts/PipePoolManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PipePoolManager : MonoBehaviour
+{
+    public static PipePoolManager Instance;
+    public GameObject pipePrefab;
+    public int poolSize = 5;
+    private Queue<GameObject> pool;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        pool = new Queue<GameObject>();
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject obj = Instantiate(pipePrefab);
+            obj.SetActive(false);
+            pool.Enqueue(obj);
+        }
+    }
+
+    public GameObject GetPipe()
+    {
+        if (pool.Count > 0)
+        {
+            GameObject obj = pool.Dequeue();
+            obj.SetActive(true);
+            return obj;
+        }
+        else
+        {
+            GameObject obj = Instantiate(pipePrefab);
+            obj.SetActive(true);
+            return obj;
+        }
+    }
+
+    public void ReturnPipe(GameObject obj)
+    {
+        obj.SetActive(false);
+        pool.Enqueue(obj);
+    }
+}
---
+++ GMTK-Unity/Assets/Scripts/PipeSpawnScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawnScript : MonoBehaviour
+{
+    public float initialSpawnInterval = 2f;  
+    public float minSpawnInterval = 1f;      
+    public float spawnIntervalDecrement = 0.1f; 
+    public Transform spawnPoint;
+
+    private float currentSpawnInterval;
+
+    private void Start()
+    {
+        currentSpawnInterval = initialSpawnInterval;
+        InvokeRepeating("SpawnPipe", currentSpawnInterval, currentSpawnInterval);
+    }
+
+    void SpawnPipe()
+    {
+        GameObject pipe = PipePoolManager.Instance.GetPipe();
+        pipe.transform.position = spawnPoint.position;
+        pipe.transform.rotation = Quaternion.identity;
+
+        // Giảm khoảng cách giữa các lần sinh cột
+        currentSpawnInterval = Mathf.Max(minSpawnInterval, currentSpawnInterval - spawnIntervalDecrement);
+
+        // Cập nhật thời gian cho InvokeRepeating
+        CancelInvoke();
+        InvokeRepeating("SpawnPipe", currentSpawnInterval, currentSpawnInterval);
+    }
+}
